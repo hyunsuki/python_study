5b3b288d6c4cb1fdf14b5231da3917e2f0d9afc8
root <root@localhost.localdomain>
2022-04-28 16:26:35 +0900

    stash

--- test_assert.py
+++ test_assert.py
@@ -1,6 +1,12 @@
 def main():
+    """
+    This file is for studing assert
+    """
     name = "1KimHyunsuk"
     assert name[0].isalpha(), "이름의 맨 처음은 알파벳으로 시작해주세요"
 
 if __name__ == '__main__':
+    print("="*100)
+    print(main.__doc__)
+    print("="*100)
     main()
